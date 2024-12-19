3a6c755701b12cdf593c36c99334f03d75c80751
白雾茫茫丶 <baiwumm@foxmail.com>
2024-12-19 16:32:45 +0800

    perf(loading): 优化页面加载 loading 样式

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,14 +2,15 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2024-12-06 10:31:17
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2024-12-09 09:35:12
- * @Description: 全局 Loading
+ * @LastEditTime: 2024-12-19 16:31:39
+ * @Description: 页面 Loading
  */
+import { Spinner } from '@nextui-org/react';
 
 export default function Loading() {
   return (
     <div className="flex justify-center items-center min-h-60">
-      <svg className="animate-spin h-10 w-10 mr-3" viewBox="0 0 24 24" />
+      <Spinner label="页面正在加载中..." />
     </div>
   );
 }
