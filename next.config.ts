0867b7194010ff5cddccaefb9503ae1af08558b9
白雾茫茫丶 <baiwumm@foxmail.com>
2025-11-05 18:06:47 +0800

    feat: 更改打包配置

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextIntl(nextConfig);
