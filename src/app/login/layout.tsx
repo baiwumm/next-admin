1c12b7be697bc4b61a573310029f48aabb2a0acb
白雾茫茫丶 <baiwumm@foxmail.com>
2025-11-10 14:27:12 +0800

    feat(login): 登录页新增 PrimaryColorPicker 组件

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -2,7 +2,7 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2025-10-31 10:04:10
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-11-10 08:59:21
+ * @LastEditTime: 2025-11-10 14:25:50
  * @Description: 登录页布局
  */
 'use client';
@@ -10,6 +10,7 @@
 import { useLocale } from 'next-intl'
 
 import LangSwitch from '@/components/LangSwitch'
+import PrimaryColorPicker from '@/components/PrimaryColorPicker'
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 import { type Locale } from '@/i18n/config'
 
@@ -23,6 +24,7 @@ export default function LoginLayout({ children }: LoginLayoutProps) {
     <div className="relative flex h-screen w-screen overflow-hidden justify-center items-center">
       {children}
       <div className="flex justify-center items-center fixed top-2 right-2">
+        <PrimaryColorPicker />
         <ThemeSwitcher />
         <LangSwitch locale={locale} />
       </div>
