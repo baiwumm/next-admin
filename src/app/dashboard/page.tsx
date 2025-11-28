a0a3af8f149fcbc8cae7730b8f4967eb76c13800
白雾茫茫丶 <baiwumm@foxmail.com>
2025-11-28 08:47:20 +0800

    Revert "feat: add DropdownMenu"
    This reverts commit b7505cfcc1f56079d27d28ea22c944440231fc9a.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,36 +2,17 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2025-10-10 09:35:59
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-11-27 22:06:24
+ * @LastEditTime: 2025-11-13 18:12:19
  * @Description: 控制台
  */
 import { Button } from "@heroui/react";
 import { type FC } from 'react';
 
-import Dropdown from '@/components/DropdownMenu'
-
 const Dashboard: FC = () => {
   return (
-    <Dropdown items={[
-      { 'type': 'item', 'content': 'New file', 'icon': 'ri:menu-line' },
-      {
-        'type': 'sub', 'content': 'Copy link', 'icon': 'link', 'items': [
-          { 'type': 'item', 'content': 'Copy full link', 'icon': 'link-plus' },
-          { 'type': 'item', 'content': 'Copy short link', 'icon': 'link-plus', 'iconProps': { 'size': 'md' } },
-          {
-            'type': 'sub', 'content': 'Copy colorful link', 'icon': 'link-plus', 'items': [
-              { 'type': 'item', 'content': 'red', 'props': { 'color': 'danger' } },
-              { 'type': 'item', 'content': 'green', 'props': { 'color': 'success' } },
-              { 'type': 'item', 'content': 'blue', 'props': { 'color': 'primary' } },
-            ]
-          },
-        ]
-      },
-      { 'type': 'item', 'content': 'Edit file', 'icon': 'pencil' },
-      { 'type': 'item', 'content': 'Delete file', 'icon': 'trash', 'props': { 'color': 'danger' } },
-    ]}>
-      <Button>Open Menu</Button>
-    </Dropdown>
+    <div className="flex justify-center items-center h-[500px]">
+      <Button color="primary">控制台</Button>
+    </div>
   )
 }
 export default Dashboard;
