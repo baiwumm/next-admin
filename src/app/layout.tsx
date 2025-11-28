12b1cbbadf047ccd0d35323b8559103740951e48
白雾茫茫丶 <baiwumm@foxmail.com>
2025-11-28 10:04:30 +0800

    feat: 添加统计代码

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,22 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2025-11-28 09:16:17
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-11-28 09:55:59
+ * @LastEditTime: 2025-11-28 10:02:57
  * @Description: 根布局
  */
+import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
+
 import { Providers } from "./Provider";
 
 import "./globals.css";
 import 'rsuite/dist/rsuite-no-reset.min.css';
+import { ClarityAnalytics, GoogleAnalytics, PlausibleAnalytics, UmamiAnalytics } from '@/components/Analytics';
+
+export const metadata: Metadata = {
+  title: process.env.NEXT_PUBLIC_APP_NAME,
+  description: process.env.NEXT_PUBLIC_APP_DESC,
+};
 
 export default function RootLayout({
   children,
@@ -22,10 +31,20 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://cdn.baiwumm.com/fonts/MapleMono-CN-Regular/result.css"
         />
+        {/* umami - 站点统计分析 */}
+        <UmamiAnalytics />
+        {/* Microsoft Clarity 统计代码 */}
+        <ClarityAnalytics />
+        {/* Google 统计 */}
+        <GoogleAnalytics />
+        {/* Plausible 统计代码 */}
+        <PlausibleAnalytics />
       </head>
       <body>
         <Providers>
           {children}
+          {/* vercel Web Analytics */}
+          <Analytics />
         </Providers>
       </body>
     </html>
