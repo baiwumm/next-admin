1e564b9da9b4137c3e5c7e0bf0624a713b2ee642
白雾茫茫丶 <baiwumm@foxmail.com>
2025-12-31 09:25:38 +0800

    feat(About): 新增“项目信息”

---
+++ src/app/about/components/ProjectInfo.tsx
@@ -0,0 +1,44 @@
+/*
+ * @Author: 白雾茫茫丶<baiwumm.com>
+ * @Date: 2025-12-30 18:13:47
+ * @LastEditors: 白雾茫茫丶<baiwumm.com>
+ * @LastEditTime: 2025-12-31 09:23:11
+ * @Description: 项目信息
+ */
+'use client'
+import { useTranslations } from 'next-intl';
+import { type FC } from 'react';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui';
+import pkg from '#/package.json';
+
+type Item = { name: string, value: string }
+
+const ProjectInfo: FC = () => {
+  const t = useTranslations('Pages.About');
+
+  const items: Item[] = [
+    { name: t('name'), value: process.env.NEXT_PUBLIC_APP_NAME! },
+    { name: t('version'), value: pkg.version },
+    { name: t('build-time'), value: process.env.NEXT_PUBLIC_BUILD_TIME! }
+  ]
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{t('project-info')}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+          {items.map(({ name, value }, index) => (
+            <div key={index} className="flex items-center border rounded-lg text-sm gap-4">
+              <div className="text-secondary-foreground bg-secondary flex justify-center items-center p-3 shrink-0">{name}</div>
+              <div className="font-bold min-w-0 flex-1">{value}</div>
+            </div>
+          ))}
+
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+export default ProjectInfo;
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,14 +2,18 @@
  * @Author: 白雾茫茫丶<baiwumm.com>
  * @Date: 2025-12-30 16:53:10
  * @LastEditors: 白雾茫茫丶<baiwumm.com>
- * @LastEditTime: 2025-12-30 17:59:29
+ * @LastEditTime: 2025-12-30 18:12:59
  * @Description: 关于
  */
 import DependenciesCard from './components/DependenciesCard';
+import ProjectInfo from './components/ProjectInfo';
 
 export default function About() {
+  // 项目信息
   return (
     <div className="flex flex-col gap-4">
+      {/* 项目信息 */}
+      <ProjectInfo />
       {/* 生产依赖 */}
       <DependenciesCard fieldName="dependencies" />
       {/* 开发依赖 */}
